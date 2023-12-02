7e813add758a61a8456e50af2a259936364953f5
Seto Balian <seto.balian@gmail.com>
2023-12-02 19:26:27 +0400

    Improve day 2

--- 02/run.py
+++ 02/run.py
@@ -1,51 +1,56 @@
+import math
 import pathlib
+import re
+from typing import Generator
 
-MAX_CUBES = {"red": 12, "green": 13, "blue": 14}
-
-
-def is_game_possible(game_line: str) -> tuple[bool, int]:
-    left, right = game_line.split(":")
-    id_ = int(left.replace("Game ", ""))
-    cube_counts = right.strip().replace(",", "").replace(";", "").split()
-    for i in range(0, len(cube_counts), 2):
-        count_ = int(cube_counts[i])
-        color = cube_counts[i + 1]
-        if count_ > MAX_CUBES[color]:
-            return False, id_
-    return True, id_
+LIMITS = {"red": 12, "green": 13, "blue": 14}
 
 
-def part1() -> int:
+def read_games(path: str) -> list[str]:
+    return pathlib.Path(path).read_text().splitlines()
+
+
+def cube_counts(game_line: str) -> Generator[tuple[int, str], None, None]:
+    for match in re.finditer(r"\d+\s(red|blue|green)", game_line):
+        count_, color = match[0].split(" ")
+        yield (int(count_), color)
+
+
+def game_possible(game_line: str) -> bool:
+    for count_, color in cube_counts(game_line):
+        if count_ > LIMITS[color]:
+            return False
+    return True
+
+
+def part1(game_lines: list[str]) -> int:
     answer = 0
-    for line in pathlib.Path("input.txt").read_text().splitlines():
-        possible, id_ = is_game_possible(line)
-        if possible:
+    # Game ids are just 1, 2, 3, ...
+    for id_, game_line in enumerate(game_lines, start=1):
+        if game_possible(game_line):
             answer += id_
     return answer
 
 
 def min_power(game_line: str) -> int:
-    _, right = game_line.split(":")
-    cube_counts = right.strip().replace(",", "").replace(";", "").split()
-    max_ = {"red": 0, "blue": 0, "green": 0}
-    for i in range(0, len(cube_counts), 2):
-        count_ = int(cube_counts[i])
-        color = cube_counts[i + 1]
+    max_ = {"red": 0, "green": 0, "blue": 0}
+    for count_, color in cube_counts(game_line):
         if count_ > max_[color]:
             max_[color] = count_
-    return max_["red"] * max_["blue"] * max_["green"]
+    return math.prod(max_.values())
 
 
-def part2() -> int:
+def part2(game_lines: list[str]) -> int:
     answer = 0
-    for line in pathlib.Path("input.txt").read_text().splitlines():
-        answer += min_power(line)
+    for game_line in game_lines:
+        answer += min_power(game_line)
     return answer
 
 
 def main() -> None:
-    assert part1() == 2563
-    assert part2() == 70768
+    game_lines = read_games("input.txt")
+    assert part1(game_lines) == 2563
+    assert part2(game_lines) == 70768
     print("All tests passed.")
 
 
