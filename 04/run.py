4950099e2c46d63d6632e8ea69e4134278e4e621
Seto Balian <seto.balian@gmail.com>
2023-12-04 22:55:20 +0000

    Day 4 part 2

--- 04/run.py
+++ 04/run.py
@@ -1,5 +1,46 @@
+import dataclasses
 import pathlib
 import re
+
+
+@dataclasses.dataclass
+class Card:
+    card_number: int
+    num_winning_numbers: int
+    num_my_numbers: int
+    winning_numbers: list[int]
+    my_numbers: list[int]
+    num_matches: int = dataclasses.field(init=False)
+    points: int = dataclasses.field(init=False)
+
+    def __post_init__(self):
+        self.num_matches = len(
+            set(self.winning_numbers).intersection(set(self.my_numbers))
+        )
+        self.points = (
+            2 ** (self.num_matches - 1) if self.num_matches != 0 else 0
+        )
+
+    @classmethod
+    def from_dict(
+        cls,
+        data: dict[str, str],
+        num_winning_numbers: int,
+        num_my_numbers: int,
+    ):
+        winning_numbers = []
+        my_numbers = []
+        for i in range(num_winning_numbers):
+            winning_numbers.append(int(data[f"winning_number_{i}"]))
+        for i in range(num_my_numbers):
+            my_numbers.append(int(data[f"my_number_{i}"]))
+        return cls(
+            int(data["card_number"]),
+            num_winning_numbers,
+            num_my_numbers,
+            winning_numbers,
+            my_numbers,
+        )
 
 
 def build_pattern(num_winning_numbers, num_my_numbers) -> str:
@@ -13,7 +54,7 @@ def build_pattern(num_winning_numbers, num_my_numbers) -> str:
         [rf"\s+(?P<my_number_{i}>\d+)" for i in range(num_my_numbers)]
     )
     return (
-        r"^Card\s+(?P<card_id>\d+):\s+"
+        r"^Card\s+(?P<card_number>\d+):\s+"
         + winning_numbers
         + r"\|"
         + my_numbers
@@ -21,34 +62,45 @@ def build_pattern(num_winning_numbers, num_my_numbers) -> str:
     )
 
 
-def read_cards(path: str) -> str:
-    return pathlib.Path(path).read_text()
+def read_cards(
+    path: str, num_winning_numbers: int, num_my_numbers: int
+) -> dict[int, Card]:
+    cards = {}
+    for m in re.finditer(
+        build_pattern(num_winning_numbers, num_my_numbers),
+        pathlib.Path(path).read_text(),
+        re.MULTILINE,
+    ):
+        card = Card.from_dict(
+            m.groupdict(), num_winning_numbers, num_my_numbers
+        )
+        cards[card.card_number] = card
+    return cards
 
 
-def part1(cards: str, num_winning_numbers, num_my_numbers) -> int:
-    pattern = build_pattern(num_winning_numbers, num_my_numbers)
-    answer = 0
-    for m in re.finditer(pattern, cards, re.MULTILINE):
-        winning_numbers = set()
-        my_numbers = set()
-        group_dict = m.groupdict()
-        for i in range(num_winning_numbers):
-            winning_numbers.add(group_dict[f"winning_number_{i}"])
-        for i in range(num_my_numbers):
-            my_numbers.add(group_dict[f"my_number_{i}"])
-        num_matches = len(winning_numbers.intersection(my_numbers))
-        if num_matches > 0:
-            answer += 2 ** (num_matches - 1)
+def total_points(cards: dict[int, Card]) -> int:
+    return sum(card.points for card in cards.values())
 
-    return answer
+
+def processed_cards(cards: dict[int, Card]) -> int:
+    processed = 0
+    stack = list(cards.values())
+    while stack:
+        card = stack.pop()
+        processed += 1
+        for i in range(card.num_matches):
+            stack.append(cards[card.card_number + i + 1])
+    return processed
 
 
 def main() -> None:
-    cards = read_cards("example.txt")
-    assert part1(cards, 5, 8) == 13
+    cards = read_cards("example.txt", 5, 8)
+    assert total_points(cards) == 13
+    assert processed_cards(cards) == 30
+    cards = read_cards("input.txt", 10, 25)
+    assert total_points(cards) == 20667
+    processed_cards(cards) == 5833065
     print("All tests passed.")
-    cards = read_cards("input.txt")
-    assert part1(cards, 10, 25) == 20667
 
 
 if __name__ == "__main__":
