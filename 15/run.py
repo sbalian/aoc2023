51648311687a3bacd8e673ef4067d55107b60218
Seto Balian <seto.balian@gmail.com>
2023-12-16 04:47:31 +0000

    Day 15 part 2

--- 15/run.py
+++ 15/run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import collections
 import pathlib
 
 from rich import print
@@ -14,15 +15,56 @@ def hash(string):
     return current_value
 
 
-def part1(path):
-    return sum(
-        hash(step) for step in pathlib.Path(path).read_text().split(",")
-    )
+def read_steps(path):
+    return pathlib.Path(path).read_text().split(",")
+
+
+def part1(steps):
+    return sum(hash(step) for step in steps)
+
+
+def part2(steps):
+    boxes = collections.defaultdict(list)
+    focal_lengths = {}
+
+    for step in steps:
+        match step.endswith("-"):
+            case True:
+                lens = step[:-1]
+                box = hash(lens)
+                if lens in boxes[box]:
+                    boxes[box].remove(lens)
+            case _:  # =
+                lens = step[:-2]
+                focal_length = int(step[-1])
+                box = hash(lens)
+                if lens not in boxes[box]:
+                    boxes[box].append(lens)
+                    focal_lengths[(lens, box)] = focal_length
+                else:
+                    focal_lengths[(lens, box)] = focal_length
+
+    lenses = {}
+    for box, lenses_ in boxes.items():
+        for i, lens in enumerate(lenses_):
+            lenses[lens] = (box, i + 1)
+
+    focusing_power = 0
+    for lens in lenses:
+        box, slot = lenses[lens]
+        focusing_power += (1 + box) * (slot) * focal_lengths[(lens, box)]
+
+    return focusing_power
 
 
 def main():
-    assert part1("example.txt") == 1320
-    assert part1("input.txt") == 510273
+    steps = read_steps("example.txt")
+    assert part1(steps) == 1320
+    assert part2(steps) == 145
+
+    steps = read_steps("input.txt")
+    assert part1(steps) == 510273
+    assert part2(steps) == 212449
     print("All tests passed.")
 
 
